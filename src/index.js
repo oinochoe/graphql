3e9dc186844f3ec556d26be5b954437dc4427177
Yeongmin Kim <oinochoe@naver.com>
2020-03-20 02:55:38 +0900

    globalstyle 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ApolloProvider } from "react-apollo";
 import client from "./apollo";
+import "./globalStyles";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
