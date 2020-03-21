aeccac49a2e4ee9071677e409d0e641ae40f7845
Yeongmin Kim <oinochoe@naver.com>
2020-03-22 00:19:20 +0900

    라우트 정리

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Notes from "../../Routes/Notes";
+import Note from "../../Routes/Note";
+import Edit from "../../Routes/Edit";
+import Add from "../../Routes/Add";
 
 import { Query } from "react-apollo";
 import { GET_NOTES } from "../../queries";
@@ -11,6 +14,9 @@ class App extends Component {
 			<BrowserRouter>
 				<Switch>
 					<Route exact={true} path={"/"} component={Notes} />
+					<Route path={"/Add"} component={Add} />
+					<Route path={"/note/:id"} component={Note} />
+					<Route path={"/edit/:id"} component={Edit} />
 				</Switch>
 			</BrowserRouter>
 		);
