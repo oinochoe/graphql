fc4570c59459097017e9099c53c67cf7d15fd2dd
Yeongmin Kim <oinochoe@naver.com>
2020-03-21 19:55:15 +0900

    cache 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+// import { GET_NOTES } from "./queries";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        {/* <Query query={GET_NOTES}>{() => null}</Query> */}
+      </div>
+    );
   }
 }
 
